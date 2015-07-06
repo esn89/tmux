ca26ee1c2a0b9bdecd3e225a715d3ef929bc53b4
fenriz <fenriz@freya>
2015-07-06 12:32:12 +0100

    panes do not wrap

--- window.c
+++ window.c
@@ -1204,7 +1204,7 @@ window_pane_find_up(struct window_pane *wp)
 
 	edge = wp->yoff;
 	if (edge == 0)
-		edge = wp->window->sy + 1;
+		return wp;
 
 	left = wp->xoff;
 	right = wp->xoff + wp->sx;
@@ -1250,7 +1250,7 @@ window_pane_find_down(struct window_pane *wp)
 
 	edge = wp->yoff + wp->sy + 1;
 	if (edge >= wp->window->sy)
-		edge = 0;
+		return wp;
 
 	left = wp->xoff;
 	right = wp->xoff + wp->sx;
@@ -1296,7 +1296,7 @@ window_pane_find_left(struct window_pane *wp)
 
 	edge = wp->xoff;
 	if (edge == 0)
-		edge = wp->window->sx + 1;
+		return wp;
 
 	top = wp->yoff;
 	bottom = wp->yoff + wp->sy;
@@ -1342,7 +1342,7 @@ window_pane_find_right(struct window_pane *wp)
 
 	edge = wp->xoff + wp->sx + 1;
 	if (edge >= wp->window->sx)
-		edge = 0;
+		return wp;
 
 	top = wp->yoff;
 	bottom = wp->yoff + wp->sy;
